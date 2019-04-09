9a0fa1d889ba63706f37417cd872d01bc5debef3
Jasterix C <loune.calixte@gmail.com>
2019-04-09 19:50:23 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  i = 0
+  while i < collection.length
+    #yield #returned array contains the same elements as the original collection (FAILED - 2)
+    #yield i #returned array contains the same elements as the original collection (FAILED - 2)
+    yield(collection[i])
+  i+=1
+  end
+  collection
+end
+
+#at this point, I got a no block given (yield) error on repl
+
+my_each(collection) do |i|
+  puts i
 end
