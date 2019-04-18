5c73898b1c1edfbe5c819875b8546d99d5ae7455
Chloe <gingkolane@gmail.com>
2019-04-18 11:47:27 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+
+  i = 0
+  while i < arr.length
+    # this is the key of building yield, passing each element in the array yield to the block yield is going to do
+    yield arr[i]
+
+    i = i + 1
+  end
+
+  # add this so my_each method returns the original arr, otherwise it returns nothing
+  arr
+
 end
